e259340a2e2b8a4fd6a3e142345839bc74689517
pascalgage <72741160+pascalgage@users.noreply.github.com>
2021-12-05 20:28:27 +0100

    en cours une colonne calculée
    ....encours

--- Employees/assets/js/dbEmployees.js
+++ Employees/assets/js/dbEmployees.js
@@ -3,7 +3,10 @@ import { Db } from './Db.js';
 class DbEmployees extends Db 
 {
 
-    
+    getSalary(employee){
+        let salary=employee.employee_salary/12;
+        return Math.round(salary);
+    }
     
     
 
--- Employees/assets/js/main.js
+++ Employees/assets/js/main.js
@@ -26,6 +26,8 @@ const employeeApp = {
 
     methods: {
 
+        
+
     }
 
 }
