b5ea5732a2149fec68687d7aae085e5ea2d0250a
pascalgage <72741160+pascalgage@users.noreply.github.com>
2021-12-05 21:06:49 +0100

    ..en cours
    en cours...Date et salaire mensuel ok

--- Employees/assets/js/dbEmployees.js
+++ Employees/assets/js/dbEmployees.js
@@ -7,7 +7,11 @@ class DbEmployees extends Db
         let salary=employee.employee_salary/12;
         return Math.round(salary);
     }
-    
+    getYear(employee){
+        let madate=new Date();
+        let year=(madate.getFullYear())-(employee.employee_age);
+        return year;
+    }
     
 
    
