7fce6c00a0876b3d40f2f76ab94a491e771f6cee
Pascal GAGE <PGage@arfp.asso.fr>
2021-12-13 16:37:01 +0100

    en cours
    encours

--- Baker/baker.js
+++ Baker/baker.js
@@ -1,7 +1,29 @@
-
-
 const myBaker = {
     
+    data(){
+        return {
+            isOpen: false
+        }
+    },
+    
+    mounted(){
+
+    },
+
+    computed:{
+
+    },
+
+    methods:{
+        //on souhaite changer le text du bouton! quand clické le texte passe de OUVRIR LA BOULANGERIE à FERMER lA BOULANGERIE
+        openClose(){
+             this.isOpen = !this.isOpen; 
+        }
+    }
+
+
+
+
 }
 
 const appBaker = Vue.createApp(myBaker);
