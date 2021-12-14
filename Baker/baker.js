837c4d3d30e96ad3d216f1fe38de747f3faf2686
Pascal GAGE <PGage@arfp.asso.fr>
2021-12-14 13:33:33 +0100

    ok
    ok

--- Baker/baker.js
+++ Baker/baker.js
@@ -2,7 +2,14 @@ const myBaker = {
     
     data(){
         return {
-            isOpen: false
+            isOpen: false,
+            niv:'0',
+            moulins:'0',
+            goldlevel:'5',
+            farine:'400',
+            baguette:'0',
+            b1gold:'0',
+            b2gold:'0'
         }
     },
     
@@ -11,14 +18,23 @@ const myBaker = {
     },
 
     computed:{
-
+        
     },
 
     methods:{
         //on souhaite changer le text du bouton! quand clické le texte passe de OUVRIR LA BOULANGERIE à FERMER lA BOULANGERIE
         openClose(){
-             this.isOpen = !this.isOpen; 
+             this.isOpen = !this.isOpen;
+             
+        },
+        start(){
+            setInterval(this.increase,1000);
+            
+        },
+        increase(){
+            this.goldlevel++;
         }
+        
     }
 
 
