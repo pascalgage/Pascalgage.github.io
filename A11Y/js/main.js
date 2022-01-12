1efa08b9dce82f523fe2ea621aeaec48539ba36a
Pascal GAGE <PGage@arfp.asso.fr>
2022-01-12 15:11:31 +0100

    ok
    ok

--- A11Y/js/main.js
+++ A11Y/js/main.js
@@ -7,13 +7,13 @@ function toggleForm(){
         if(etatform =='none'){
             
             monform.style.display='block';
-            
+            console.log('ok');
             button.innerHTML='Déclarer un incident ▲';
             button.setAttribute('aria-expanded',true);
         }
         else{
             monform.style.display='none';
-            
+            console.log('ok');
             button.innerHTML='Déclarer un incident ▼';
             button.setAttribute('aria-expanded',false);
         }
---
+++ Try/js/main.js
@@ -0,0 +1,30 @@
+
+function toggleOpenClose(){
+    var formul=document.getElementById('mai');
+    
+    var etat=formul.style.display;
+
+    if(etat=='none'){
+        formul.style.display='flex';
+    }else{
+        formul.style.display='none';
+    }
+
+
+};
+
+function toggle(){
+    var formul=document.getElementById('mai');
+    
+    var etat=formul.style.display;
+
+    if(etat=='none'){
+        formul.style.display='flex';
+    }else{
+        formul.style.display='none';
+    }
+}
+
+ 
+
+
