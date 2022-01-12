43189b0cd92b41ca6bdc4b6d3fb37cd08b13fce6
Gage <72741160+pascalgage@users.noreply.github.com>
2022-01-12 20:18:21 +0100

    ok
    ok

---
+++ Try/js/main2.js
@@ -0,0 +1,12 @@
+var nav = document.getElementById('main');
+
+function toggleClose(){
+    nav.classList.remove('scooper');
+    nav.classList.add('active');
+}
+
+function toggle(){
+    nav.classList.remove('active');
+    nav.classList.add('scooper');
+}
+
