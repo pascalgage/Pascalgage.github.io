a20d9320c8b17a2da000f41d1877d24ab032264d
Pascal GAGE <PGage@arfp.asso.fr>
2022-01-18 12:20:12 +0100

    MVC intro
    ok

---
+++ IntroMVC/IntroMVC/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Toto()
+        {
+            return "<h1>SALUT les Copains !!!!</h1>";
+        }
+
+        public IActionResult Barry()
+        {
+            return View();
+        }
+    }
+}
