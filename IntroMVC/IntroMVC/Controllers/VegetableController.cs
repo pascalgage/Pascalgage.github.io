99d2c669d2208702385bf1f41cda08435f439c84
Pascal GAGE <PGage@arfp.asso.fr>
2022-01-18 15:54:45 +0100

    ok
    ok

---
+++ IntroMVC/IntroMVC/Controllers/VegetableController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class VegetableController : Controller
+    {
+        // GET: VegetableController
+        //afficher un élément par son identifiant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VegetableController/Details/5
+        //créer un nouvel élément
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VegetableController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VegetableController/Create
+        //validation du formulaire Create (l'action ci dessus)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VegetableController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VegetableController/Edit/5
+        //validation du formulaire d'édition
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VegetableController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VegetableController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ IntroMVC/IntroMVC/Models/Vegetable.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroMVC.Models
+{
+    public class Vegetable
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Family { get; set; }
+        public double Price { get; set; }
+    }
+}
