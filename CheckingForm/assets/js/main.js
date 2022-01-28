6f1f3d23cd86b6c426d62aa118a1c2609d840f49
pascalgage <72741160+pascalgage@users.noreply.github.com>
2022-01-29 00:24:25 +0100

    ok
    ok

---
+++ CheckingForm/assets/js/main.js
@@ -0,0 +1,32 @@
+
+function checkInput(){
+
+    var pasOK = document.getElementById('test1');
+    var ok = document.getElementById('test2');
+    const champ = document.getElementById('insert');
+    const champValue = champ.value.trim(); 
+    var army=false;
+
+
+
+    if(champValue===" "){
+        
+        army=false;
+
+    }
+    else if(champValue>=0)
+    {
+        
+        army=true;
+        
+    }
+    if(army===false){
+        pasOK.classList.remove('fax1');
+        pasOK.classList.add('notest');
+    }else{
+        ok.classList.remove('fax2');
+        ok.classList.add('notest');
+    }
+        
+        
+}
