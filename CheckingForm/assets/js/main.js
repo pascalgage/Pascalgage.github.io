52951075a7d612ad4f60eb110d1c4870bf8c053f
pascalgage <72741160+pascalgage@users.noreply.github.com>
2022-01-29 01:19:30 +0100

    exp ternaire
    avec exp ternaire

--- CheckingForm/assets/js/main.js
+++ CheckingForm/assets/js/main.js
@@ -3,36 +3,29 @@ function checkInput(){
 
     var pasOK = document.getElementById('test1');
     var ok = document.getElementById('test2');
-    const champ = document.getElementById('insert');
 
+    const champ = document.getElementById('insert');
     const champValue = champ.value.trim(); 
 
     var bool=false;
+    
+    (champValue===" ")? bool=false : bool=true;
+    (champValue>=0)? bool=true : bool=false;
 
-    if(champValue===" "){
-        
-        bool=false;
-
-    }
-    else if(champValue>=0)
-    {
-        
-        bool=true;
-        
-    }
+    
 
     if(bool===false)
     {
         pasOK.classList.remove('fax1');
         pasOK.classList.add('notest');
-        ok.classList.remove('notest');
+        ok.classList.remove('notest2');
         ok.classList.add('fax2');
         
     }
     else
     {
         ok.classList.remove('fax2');
-        ok.classList.add('notest');
+        ok.classList.add('notest2');
         pasOK.classList.remove('notest');
         pasOK.classList.add('fax1');
         
