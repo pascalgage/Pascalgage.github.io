707d26b355d9c57ea65f1ed1494b6b81f0b0601c
pascalgage <72741160+pascalgage@users.noreply.github.com>
2022-01-29 16:18:58 +0100

    en cours
    en cours...;

--- CheckingForm/assets/js/main.js
+++ CheckingForm/assets/js/main.js
@@ -7,20 +7,20 @@ function checkInput(){
     const champ = document.getElementById('insert');
     const champValue = champ.value.trim(); 
 
-    var bool=false;
+    var boolName=false;
     
-    (champValue===" ")? bool=false : bool=true;
-    (champValue>=0)? bool=true : bool=false;
+    (champValue===" ")? boolName=false : boolName=true;
+    (champValue>=0)? boolName=true : boolName=false;
 
     
 
-    if(bool===false)
+    if(boolName===false)
     {
         pasOK.classList.remove('fax1');
         pasOK.classList.add('notest');
         ok.classList.remove('notest2');
         ok.classList.add('fax2');
-        
+        setErrorFor(champ,'');
     }
     else
     {
@@ -28,9 +28,50 @@ function checkInput(){
         ok.classList.add('notest2');
         pasOK.classList.remove('notest');
         pasOK.classList.add('fax1');
+        setErrorFor(champ,'Veuillez saisir au moins un caractère');
         
-    }
-    
+    }     
+}
 
-        
+function setErrorFor(input, message) {
+	
+    const formControl=document.getElementById('smallmessage');
+	formControl.innerText = message;
+
+}
+function setErrorForMail(input, message) {
+	
+    const formControl=document.getElementById('smallmessageMail');
+	formControl.innerText = message;
+
+}
+
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function checkInputMail(){
+    var pasOK = document.getElementById('test3');
+    var ok = document.getElementById('test4');
+    const champ2 = document.getElementById('insertMail');
+    const champValue2 = champ2.value.trim();
+    var boolMail=false;
+
+    (champValue2!='' && isEmail(champValue2))?boolMail=false : boolMail=true;
+
+    if(boolMail==false){
+        pasOK.classList.remove('fax1');
+        pasOK.classList.add('notest');
+        ok.classList.remove('notest2');
+        ok.classList.add('fax2');
+        setErrorForMail(champ2,'');
+    }else{
+        ok.classList.remove('fax2');
+        ok.classList.add('notest2');
+        pasOK.classList.remove('notest');
+        pasOK.classList.add('fax1');
+        setErrorForMail(champ2,'Email invalide');
+    }
+
 }
