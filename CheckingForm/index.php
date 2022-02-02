24ea0c7485868553e1650ead2276544e1cbbb47b
Pascal GAGE <PGage@arfp.asso.fr>
2022-02-02 12:36:23 +0100

    Problème résolu
    ok fixed !!!

--- CheckingForm/index.php
+++ CheckingForm/index.php
@@ -15,7 +15,7 @@
 
     </header>
     <main>
-        <form action="" method="POST">
+        <form action="index.php" method="POST">
         <div class="container" id="form">
                     <div class="buttonClose">
                         <button type="button" id="buttonX" onclick="closeForm()">X</button>
@@ -90,10 +90,9 @@ if(isset($_POST['submit'])){
 
     if(!empty($_POST['password'])){
 
-    
-
     $insertDATA = $bdd->prepare("INSERT INTO trolkax(username, email, pass, password2) VALUES (?,?,?,?)");
     $insertDATA->execute(array($username, $email, $password, $password2));
+    header('Location: followinscription.html');
 }
 }
 
