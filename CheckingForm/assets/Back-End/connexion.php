8d79885369fae3d2fad9aae3cab221a58c2af728
Pascal GAGE <PGage@arfp.asso.fr>
2022-02-16 10:04:32 +0100

    en cours
    encours

---
+++ CheckingForm/assets/Back-End/connexion.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+ 
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=trolka', 'root', '');
+ 
+if(isset($_POST['formconnexion'])) {
+   $mailconnect = htmlspecialchars($_POST['mailconnect']);
+   $mdpconnect = sha1($_POST['mdpconnect']);
+   if(!empty($mailconnect) AND !empty($mdpconnect)) {
+      $requser = $bdd->prepare("SELECT * FROM trolkax WHERE email = ? AND pass = ?");
+      $requser->execute(array($mailconnect, $mdpconnect));
+      $userexist = $requser->rowCount();
+      if($userexist == 1) {
+         $userinfo = $requser->fetch();
+         $_SESSION['id'] = $userinfo['id'];
+         $_SESSION['pseudo'] = $userinfo['pseudo'];
+         $_SESSION['mail'] = $userinfo['mail'];
+         header("Location: firstPAGE.html?id=".$_SESSION['id']);
+      } else {
+         $erreur = "Mauvais mail ou mot de passe !";
+      }
+   } else {
+      $erreur = "Tous les champs doivent être complétés !";
+   }
+}
+?>
