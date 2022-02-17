d51a3683e95f5393bab18be465122f593b4296d1
Pascal GAGE <PGage@arfp.asso.fr>
2022-02-17 15:42:56 +0100

    Update sendToDatabase.php
    ok

--- CheckingForm/assets/Back-End/sendToDatabase.php
+++ CheckingForm/assets/Back-End/sendToDatabase.php
@@ -9,15 +9,18 @@ if(isset($_POST['submit'])){
     $email=htmlspecialchars($_POST['email']);
     $password= sha1($_POST['password']);
     $password2= sha1($_POST['password2']);
+    $namelength=strlen($username);
+    
 
     if(!empty($_POST['nom'])&&!empty($_POST['email'])&&!empty($_POST['password'])&&!empty($_POST['password2'])){
-        if(filter_var($email, FILTER_VALIDATE_EMAIL))
-			{
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
 			$reqmail = $bdd->prepare("SELECT * FROM trolkax WHERE email = ?");
 			$reqmail->execute(array($email));
 			$mailexist = $reqmail->rowcount();
             if($mailexist == 0){
-
+                if($namelength <= 255){
+                    
+                        
     $insertDATA = $bdd->prepare("INSERT INTO trolkax(username, email, pass, password2) VALUES (?,?,?,?)");
     $insertDATA->execute(array($username, $email, $password, $password2));
     header("Location: follow.html");
@@ -26,11 +29,18 @@ if(isset($_POST['submit'])){
 }else{
     header("Location: errorI.php");
 }
+
+    
+
 }else{
     header("Location: errorI.php");
 }
 }else{
     header("Location: errorI.php");
 }
+}else{
+    header("Location: errorI.php");
+}
+
 }
 ?>
