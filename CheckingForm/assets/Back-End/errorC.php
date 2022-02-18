5d38614d74e9efab181b872c908e4e12d72e98f8
Pascal GAGE <PGage@arfp.asso.fr>
2022-02-18 08:49:31 +0100

    en cours
    encours

--- CheckingForm/assets/Back-End/errorC.php
+++ CheckingForm/assets/Back-End/errorC.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/errorC.css">
     <title>ERROR SYSTEM</title>
 </head>
 
 <body>
     <form action="/index.html">
-    <h1>ERREUR DE CONNEXION !</h1>
+    <h1>ERREUR DE CONNEXION !</h1><br>
     <button type="submit" >RETOURNER à la PAGE D'ACCUEIL</button>
     </form>
 </body>
--- CheckingForm/assets/Back-End/errorI.php
+++ CheckingForm/assets/Back-End/errorI.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/errorI.css">
     <title>ERROR SYSTEM</title>
 </head>
 <body>
-    <h1>ERREUR D'INSCRIPTION</h1>
     <form action="/index.html">
+    <h1>ERREUR D'INSCRIPTION</h1>
     <button type="submit" >RETOURNER à la PAGE D'ACCUEIL</button>
     </form>
 </body>
